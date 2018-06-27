13f26d95e9c48f55881922841f0e670893b1ec0e
M416 <chanon_w@cmu.ac.th>
2018-06-27 11:43:03 +0700

    creat homework

---
+++ hw-jquery.js
@@ -0,0 +1,18 @@
+$("#banner-secondary *").hide()
+$("#banner-secondary").html('\
+	<input type="text" placeholder="Enter Username" id="uname" required>\
+ <input type="text" placeholder="Enter Passs" id="pname" required> <br>\
+ <button type="submit" id="sbt">Login</button>\
+ ')
+
+$("#sbt").click(function(){
+	if($(uname).val() == 'bananacoding' ){
+		if($(pname).val() == 'password' ){
+			alert("Hello! Login success!!");
+		}else
+		{
+			alert("Login again!!");
+		}
+	}
+	
+});
