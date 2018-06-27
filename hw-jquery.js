be102fbe17dd225648db6958352492e6abace6d0
chanon1261 <chanon_w@cmu.ac.th>
2018-06-27 11:53:21 +0700

    Update hw-jquery.js

--- hw-jquery.js
+++ hw-jquery.js
@@ -1,7 +1,7 @@
 $("#banner-secondary *").hide()
 $("#banner-secondary").html('\
   <input type="text" placeholder="Enter Username" id="uname" required>\
-  <input type="password" placeholder="Enter Passs" id="pname" required> <br>\
+  <input type="text" placeholder="Enter Passs" id="pname" required> <br>\
   <button type="submit" id="sbt">Login</button>\
   ')
 
